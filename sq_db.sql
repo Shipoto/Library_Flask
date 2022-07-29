cd6a6b7d5e3bfdd87787a70b61d48955173a4345
Big <whisper888@bk.ru>
2022-07-29 11:46:32 +0300

    rename lib-site.py to lib_site.py

--- sq_db.sql
+++ sq_db.sql
@@ -9,7 +9,8 @@ id integer PRIMARY KEY AUTOINCREMENT,
 title text NOT NULL,
 text text NOT NULL,
 url text NOT NULL,
-image text NOT NULL,
+image BLOB DEFAULT NULL,
+user_id integer NOT NULL,
 time integer NOT NULL
 );
 
@@ -18,5 +19,6 @@ id integer PRIMARY KEY AUTOINCREMENT,
 username text NOT NULL,
 email text NOT NULL,
 psw text NOT NULL,
+avatar BLOB DEFAULT NULL,
 time integer NOT NULL
 );
